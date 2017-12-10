9e40b5e4cdf3e0b0ed20e8630f5accf27c9f1785
giodiblasi <gio.diblasi@gmail.com>
2017-12-10 13:30:31 +0100

    renaming

--- tailRecursion/index.js
+++ tailRecursion/index.js
@@ -1,15 +1,15 @@
 const tailFibonacci = require('./tailFibonacci');
 const headFibonacci = require('./headFibonacci');
 
-const getGenerators = (generatorType, number) => {
-  if (generatorType === 'tail') return tailFibonacci.getFibonacciNumber(number);
-  if (generatorType === 'head') return headFibonacci.getFibonacciNumber(number);
-  return () => {};
+const getFibonacciNumber = (recursionType, number) => {
+  if (recursionType === 'tail') return tailFibonacci.getFibonacciNumber(number);
+  if (recursionType === 'head') return headFibonacci.getFibonacciNumber(number);
+  return `"${recursionType}" is not a valid type, use "tail" or "head"`;
 };
 
 const type = process.argv[2];
 const number = process.argv[3];
 
-console.log(getGenerators(type, number));
+console.log(getFibonacciNumber(type, number));
 
 // node  --harmony  --use-strict tailRecursion\index.js [tail|head] 10
