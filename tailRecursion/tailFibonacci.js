f699e602f0ce7433a84cd94d928abb735464a108
giodiblasi <gio.diblasi@gmail.com>
2017-12-11 10:04:00 +0100

    updated readme

--- tailRecursion/tailFibonacci.js
+++ tailRecursion/tailFibonacci.js
@@ -2,7 +2,7 @@
 const tailFibonacciSequence = (n, n1, n2) => {
   if (n === 0) return n2;
   const nextN1 = n2;
-  const nextN2 = n1 + n2;
+  const nextN2 = (n1 + n2);
   return tailFibonacciSequence(n - 1, nextN1, nextN2);
 };
 
