3df048843fdf11aa1ea58de6690700d75bdc7e8f
giodiblasi <gio.diblasi@gmail.com>
2019-08-04 21:03:19 +0200

    symbols

--- abstractData/dispatchingOnType/dispatchingOnType.js
+++ abstractData/dispatchingOnType/dispatchingOnType.js
@@ -10,42 +10,39 @@ const {
     anglePolar,
     magnitudePolar
  } = require('./complexForms');
-const tag = require('../tag');
 
+const tag = require('../tag');
+const {RECTANGULAR, POLAR} = require('./symbols');
 
 const applyByTag = (taggedComplex, methods) => {
     const typeTag = tag.getTypeTag(taggedComplex);
     const complex = tag.getContent(taggedComplex);
-    return methods[typeTag](complex)
+    const selectedMethod = methods
+        .filter(taggeMethod=>tag.getTypeTag(taggeMethod) == typeTag)
+        .map(taggeMethod=>tag.getContent(taggeMethod))[0]
+    return selectedMethod(complex);
 }
 
 const realPart = taggedComplex =>
-    applyByTag(taggedComplex,
-                {
-                    rectangular: realPartRectangular,
-                    polar: realPartPolar
-                });
+    applyByTag(taggedComplex,[
+        tag.attachTag(RECTANGULAR, realPartRectangular),
+        tag.attachTag(POLAR, realPartPolar)]);
 
 const imgPart = taggedComplex =>
-    applyByTag(taggedComplex,
-                {
-                    rectangular: imgPartRectangular,
-                    polar: imgPartPolar
-                });
+    applyByTag(taggedComplex,[
+        tag.attachTag(RECTANGULAR, imgPartRectangular),
+        tag.attachTag(POLAR, imgPartPolar)]);
+    
 
 const magnitude = taggedComplex =>
-    applyByTag(taggedComplex,
-        {
-            rectangular: magnitudeRectangular,
-            polar: magnitudePolar
-        });
-
+    applyByTag(taggedComplex,[
+        tag.attachTag(RECTANGULAR, magnitudeRectangular),
+        tag.attachTag(POLAR, magnitudePolar)]);
+    
 const angle = taggedComplex =>
-    applyByTag(taggedComplex,
-        {
-            rectangular: angleRectangular,
-            polar: anglePolar
-        });
+    applyByTag(taggedComplex,[
+        tag.attachTag(RECTANGULAR, angleRectangular),
+        tag.attachTag(POLAR, anglePolar)]);
 
 module.exports = {
     realPart,
--- abstractData/messagePassing/complexForms.js
+++ abstractData/messagePassing/complexForms.js
@@ -1,3 +1,4 @@
+const symbols = require('./symbols');
 //Rectangular Form
 
 const magnitudeRectangular = (x, y) => Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
@@ -5,12 +6,11 @@ const magnitudeRectangular = (x, y) => Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
 const angleRectangular = (x,y) => Math.atan(y / x);
 
 const makeRectangular = (x,y) => op => {
-    console.log('craated rect')
     switch(op){
-        case 'realPart': return x; 
-        case 'imgPart': return y;
-        case 'angle': console.log("angle rectangular"); return angleRectangular(x, y);
-        case 'magnitude': return magnitudeRectangular(x,y);
+        case symbols.REAL_PART: return x; 
+        case symbols.IMG_PART: return y;
+        case symbols.ANGLE: return angleRectangular(x, y);
+        case symbols.MAGNITUDE: return magnitudeRectangular(x,y);
     }
 }
 
@@ -20,12 +20,11 @@ const realPartPolar = (r,A) => r * Math.cos(A);
 const imgPartPolar = (r,A) => r * Math.sin(A);
 
 const makePolar = (r,A) => op => {
-    console.log('craated polar')
     switch(op){
-        case 'realPart': return realPartPolar(r,A); 
-        case 'imgPart': return imgPartPolar(r,A);
-        case 'angle':  console.log("angle polar"); return A;
-        case 'magnitude': return r;
+        case symbols.REAL_PART: return realPartPolar(r,A); 
+        case symbols.IMG_PART: return imgPartPolar(r,A);
+        case symbols.ANGLE:  return A;
+        case symbols.MAGNITUDE: return r;
     }
 }
 
